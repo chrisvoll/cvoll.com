46efeec99a1e9e5a1923023172b58a9462511f72
Chris Voll <chris@chrisvoll.com>
2011-08-19 22:00:39 -0400

    Added the files from the server

---
+++ index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Chris Voll</title>
+    <link href="style.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="http://use.typekit.com/hul1fnc.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-10767106-1']);
+      _gaq.push(['_trackPageview']);
+		
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+  </head>
+	
+  <body>
+    <div class="text">
+      I'm Chris Voll.
+      <small>I'm the front-end web developer at <a href="http://seatgeek.com">SeatGeek</a>.</small>
+    </div>
+
+    <div class="contact">
+      <a href="mailto:c@cvoll.com">c@cvoll.com</a> / <a href="http://twitter.com/chrisvoll">@chrisvoll</a>
+    </div>
+  </body>
+	
+</html><?php
+/*<script type="text/javascript">
+	// Show portfolio descriptions
+	function showd(obj) { document.getElementById("detail").innerHTML = obj.getAttribute('alt'); }
+	function hided()    { document.getElementById("detail").innerHTML = ""; }
+
+	// Show the portfolio when the user uses the back button
+	window.onload = function() { if(window.location.hash == "#portfolio") { document.getElementById('portfolio').className='show'; } }
+</script>*/
+
+/*I make <a href="#portfolio" onclick="document.getElementById('portfolio').className='show';">websites</a>.</div>*/
+/*<div id="portfolio">
+	<a href="http://appoftheday.com" class="r">
+		<img src="img/appoftheday.png" alt="App of the Day" onmouseover="showd(this);" onmouseout="hided();"/>
+	</a>
+
+	<a href="http://seatgeek.com/ticketportfolio" class="r">
+		<img src="img/seatgeek.png" alt="SeatGeek Ticket Portfolio" onmouseover="showd(this);" onmouseout="hided();"/>
+	</a>
+
+	<a href="http://deskconnect.com" class="r">
+		<img src="img/deskconnect.png" alt="DeskConnect" onmouseover="showd(this);" onmouseout="hided();"/>
+	</a>
+
+	<a href="http://konbitfootballayiti.org" class="r">
+		<img src="img/kfa.png" alt="Konbit Football Ayiti" onmouseover="showd(this);" onmouseout="hided();"/>
+	</a>
+
+	<a href="http://jordansatok.com" class="r">
+		<img src="img/jsatok.png" alt="Jordan Satok" onmouseover="showd(this);" onmouseout="hided();"/>
+	</a>
+</div>*/
+
+/*<div id="detail"></div>*/
+
+
+/* CSS:
+
+#portfolio {
+	position:       absolute;
+	top:            50%;
+	left:           50%;
+	right:          100%;
+	z-index:        100;
+
+	background:		  #000;
+	color:			    #FFF;
+	text-align:		  right;
+	opacity:		    0;
+
+	height:			    73px;
+	margin-top:		 -70px;
+	padding:		    20px 25px;
+
+	-webkit-transition: left .3s ease-out, right .3s ease-out, opacity .3s ease-out;
+}
+
+#portfolio.show {
+	left:			 0%;
+	right:			50%;
+	opacity:		 1 ;
+}
+
+#detail {
+	position:		    absolute;
+	top:			      50%;
+	right:			    50%;
+	margin:			    50px 25px 0 0;
+}
+
+*/
+?>
